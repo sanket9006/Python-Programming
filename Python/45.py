3f612999f27228a3543e9fda71da16292c73bf53
Sanket Patil <53385614+sanket9006@users.noreply.github.com>
2022-05-13 10:57:44 +0530

    New

--- Python/45.py
+++ Python/45.py
@@ -1,3 +1,3 @@
 from subprocess import call
 # call(["netsh", "wlan", "show", "profiles"])
-call(["netsh", "wlan", "show", "profile", "V2025_Sanket", "key", "=", "clear"])
+call(["netsh", "wlan", "show", "profile", "PHTPL", "key", "=", "clear"])
---
+++ Python/53.py
@@ -0,0 +1,9 @@
+import os
+# Access all environment variables
+print('*----------------------------------*')
+print(os.environ)
+print('*----------------------------------*')
+# Access a particular environment variable
+print(os.environ['HOME'])
+print('*----------------------------------*')
+print(os.environ['PATH'])
